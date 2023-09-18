43340f882894481afa3895de1cc352124eaf669b
Mikken2303 <pallayovci@gmail.com>
2023-09-18 17:01:30 +0200

    blob

--- head.h
+++ head.h
@@ -1,1 +1,6 @@
+#include <stdio.h>
 
+int bl(){
+    printf("blalblabla");
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#include "head.h"
 int main() {
     printf("Hello, World!\n");
+    bl();
     return 0;
 }
