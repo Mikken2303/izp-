37d6534a4809beee83222fbe3640269f003d296c
Mikken2303 <pallayovci@gmail.com>
2023-09-18 17:48:29 +0200

    blob

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "head.h"
 int main() {
-    printf("Hello, World!\n");
-    bl();
+    int num;
+    printf("daj cislo: ");
+    scanf("%d",&num);
+    if(num % 2 == 0){
+        printf("sude");
+    }
+    else{
+        printf("liche");
+    }
     return 0;
 }
